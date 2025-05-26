f8f142416cbf4e3846c38e9a5b638c19ce260dbb
MooLabsCan <155178710+MooLabsCan@users.noreply.github.com>
2025-05-26 16:04:45 -0400

    auth fixxxxxxx

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const isDev = import.meta.env.DEV
 //isDev ? '' :
 export const apiClient = axios.create({
-    baseURL: 'https://liap.ca', // Uses Vite proxy in dev, real backend in prod
+    baseURL: 'https://liap.ca/api', // Uses Vite proxy in dev, real backend in prod
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json',
